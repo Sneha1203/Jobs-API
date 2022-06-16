7d9c07a25cca4a5820ac8f250c08db0cfb305603
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-06-16 16:35:20 +0530

    register user completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,16 @@ const {BadRequestError} = require('../errors')
 
 
 const register = async (request, response) => {
-    const {name, email, password} = request.body
 
     // if(!name || !email || !password) {
     //     throw new BadRequestError('Please provide all credentials...')
     // }
 
     const user = await User.create({...request.body})
+    const token = user.createJWT()
+    // const token = jwt.sign({ userID: user._id, name: user.name }, process.env.JWT_SECRET, {expiresIn: '30d'})
 
-    response.status(StatusCodes.CREATED).json({ user })
+    response.status(StatusCodes.CREATED).json({ user: {name: user.name}, token })
 }
 
 const login = async (request, response) => {
